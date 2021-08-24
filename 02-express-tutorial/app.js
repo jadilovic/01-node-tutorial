1fedcb3734c8a30cbfd0051b9aba12731a3664f2
jadilovic <adilovic79@yahoo.com>
2021-08-24 22:17:31 +0200

    started express tutorial

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,30 @@
-console.log('Express Tutorial')
+console.log('Express Tutorial');
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const { products } = require('./data');
+
+/*
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+	res.status(200).sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+});
+*/
+
+app.get('/', (req, res) => {
+	res.json(products);
+});
+
+app.get('/about', (req, res) => {
+	res.status(200).send('About Page');
+});
+
+app.all('*', (req, res) => {
+	res.status(404).send('Error Page');
+});
+
+app.listen(5000, () => {
+	console.log('Server is running at port 5000');
+});
