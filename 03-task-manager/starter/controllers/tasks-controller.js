e37dc07a25386f0821c8b8246e6629a61799dc29
jadilovic <adilovic79@yahoo.com>
2021-08-27 23:48:22 +0200

    continuing with task manager

--- 03-task-manager/starter/controllers/tasks-controller.js
+++ 03-task-manager/starter/controllers/tasks-controller.js
@@ -35,12 +35,37 @@ const getTask = async (req, res) => {
 	}
 };
 
-const updateTask = (req, res) => {
-	res.send('update task');
+const deleteTask = async (req, res) => {
+	const { id: taskID } = req.params;
+	try {
+		const oneTask = await Task.findOneAndDelete({ _id: taskID });
+		if (!oneTask) {
+			return res
+				.status(404)
+				.json({ msg: `Task with the id ${taskID} does not exist` });
+		}
+		res.status(200).json({ oneTask });
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 };
 
-const deleteTask = (req, res) => {
-	res.send('delete task');
+const updateTask = async (req, res) => {
+	try {
+		const { id: taskID } = req.params;
+		const oneTask = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+			new: true,
+			runValidators: true,
+		});
+		if (!oneTask) {
+			return res
+				.status(404)
+				.json({ msg: `Task with the id ${taskID} does not exist` });
+		}
+		res.status(200).json({ oneTask });
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 };
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
